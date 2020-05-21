3c7c7a3a4eb0b1fdb77a6c27fc9f2bd47e2cadee
eloifabius <fabius1995>
2020-05-21 05:44:51 +0200

    implémentation des tests unitaire

---
+++ src/test/java/fr/uvsq/test/TestDrawingApp.java
@@ -0,0 +1,15 @@
+package fr.uvsq.test;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class DrawingAppTest
+{
+	@Test
+	public void test() throws SQLException {
+	}
+}
